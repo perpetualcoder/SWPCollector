4498db5f6b5d9d4c1f4635ec0c10680cd91bcbc4
Hari Krishnan <hari1106@gmail.com>
2015-09-08 16:13:43 -0500

    Test framework included.

---
+++ SWP/src/edu/lsu/cct/swp/NodeTest.java
@@ -0,0 +1,10 @@
+package edu.lsu.cct.swp;
+
+public class NodeTest extends junit.framework.TestCase {
+	public void testNothing() {
+	}
+
+	public void testWillAlwaysFail() {
+		fail("An error message");
+	}
+}
