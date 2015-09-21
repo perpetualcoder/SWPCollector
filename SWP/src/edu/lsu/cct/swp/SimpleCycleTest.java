6551400f87e0af4a10a3f51611b7012f03985332
hari <hari@hari-cct>
2015-09-21 12:13:54 -0500

    test for recovery written

--- SWP/src/edu/lsu/cct/swp/SimpleCycleTest.java
+++ SWP/src/edu/lsu/cct/swp/SimpleCycleTest.java
@@ -21,6 +21,8 @@ public class SimpleCycleTest extends junit.framework.TestCase {
 			prev = node[i];
 		}
 		node[4].createLink(node[0].getNodeId());
+		node[0].processQueue();
+		node[4].processQueue();
 		// Verifying if the nodes are all healthy and 
 		// count as expected.
 		for (int i = 0; i < 5; i++) {
@@ -49,5 +51,57 @@ public class SimpleCycleTest extends junit.framework.TestCase {
 		}
 
 	}
+	
+	public void testSimpleCyleRecover() {
+		Message.master = master;
+		Node root = master.createNode();
+		Node prev = root;
+		// Creating five nodes and then creating a 
+		// cycle.
+		for (int i = 0; i < 5; i++) {
+			node[i] = master.createNode();
+			prev.createLink(node[i].getNodeId());
+			node[i].processQueue();
+			prev.processQueue();
+			prev = node[i];
+		}
+		node[4].createLink(node[0].getNodeId());
+		node[0].processQueue();
+		node[4].processQueue();
+		root.createLink(node[2].getNodeId());
+		node[2].processQueue();
+		root.processQueue();
+		// Verifying if the nodes are all healthy and 
+		// count as expected.
+		for (int i = 0; i < 5; i++) {
+			assertTrue("The Nodes are not strong", node[i].getSRC() == 1);
+			assertTrue("The nodes are healthy",
+					node[i].getState() == NodeState.Healthy);
+		}
+		assertTrue("The node 0 weak count is not right", node[0].getWRC() == 1);
+		assertTrue(" The node 1 weak count is not right", node[2].getWRC() == 1);
+		// Severe the root link.
+		root.deleteLink(node[0].getNodeId());
+		// Process all the messages in the system.
+		while (true) {
+			boolean exit = true;
+			for (int i = 0; i < 5; i++) {
+				if (master.getNode(node[i].getNodeId()).getQu().size() > 0) {
+					exit = false;
+					master.getNode(node[i].getNodeId()).processQueue();
+				}
+			}
+			if (exit)
+				break;
+		}
+		// Verify if all the nodes are healthy by now because we have a root 
+		// node connected to one of the nodes.
+		for (int i = 0; i < 5; i++) {
+			assertTrue("Node is not dead",
+					node[i].getState() == NodeState.Healthy);
+		}
+
+	}
+
 
 }
